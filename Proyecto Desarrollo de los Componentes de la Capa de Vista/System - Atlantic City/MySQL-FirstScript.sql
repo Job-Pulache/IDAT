2483a183360b783bb06700bc89afdf793edc18d3
pulachecarrenojob <pulachecarrenojob@gmail.com>
2026-07-16 19:46:50 -0500

    add diagrama de GANTT - Ms Project

---
+++ Proyecto Desarrollo de los Componentes de la Capa de Vista/System - Atlantic City/MySQL-FirstScript.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE atlantic_city_crm;
+SHOW DATABASES;
+DROP DATABASE atlantic_city_crm;
+CREATE DATABASE atlantic_city_crm CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE atlantic_city_crm;
+SHOW TABLES;
+select * from clientes_cliente ;
+exit;
